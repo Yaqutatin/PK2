cb39909c36d0847b488a6ad974f00c2d6a4c00ea
Yaqutatin <143370825+Yaqutatin@users.noreply.github.com>
2023-09-10 16:10:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-function changeBackground() {
-    var input = document.getElementById('backgroundInput');
-    var file = input.files[0];
-    var reader = new FileReader();
-
-    reader.onload = function(e) {
-        document.body.style.backgroundImage = `url(${e.target.result})`;
-    }
-
-    reader.readAsDataURL(file);
+.container {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
 }
+
+.text {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+.left {
+  text-align: left;
+}
+
+.right {
+  text-align: right;
+}
+body {
+        color: #e6855e;
+        font-size: 30px;
+        background-color:#f3f372;
+        height: 80px;
+        width: 200px;
+      }
+      img{
+        height:50px;
+        width:70px;
+      
+      }
